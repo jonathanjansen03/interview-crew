9b79794f25fcba09a08656cbf86342e61f946094
jonathanjansen03 <jonathan.jansen03@gmail.com>
2023-01-19 01:51:20 +0700

    style: modify welcome admin,create job pages style

--- resources/views/admin/welcome.blade.php
+++ resources/views/admin/welcome.blade.php
@@ -5,9 +5,9 @@
 @section('main-content')
     <main class="relative flex justify-center align-middle">
         <div class="flex flex-col justify-center align-middle absolute bg-white drop-shadow-lg inset-y-60 w-1/4 h-64 px-6 rounded-xl">
-            <h3 class="text-black font-bold text-xl text-center">Welcome Admin!</h3>
+            <h3 class="text-black font-bold text-2xl text-center">Welcome Admin!</h3>
 
-            <div class="flex flex-col mt-5">
+            <div class="flex flex-col">
                 <div class="flex flex-col align-middle">
                     <button type="submit" class="w-full flex items-center justify-center px-1 py-0 border border-transparent text-base font-medium rounded-md mt-5 text-white bg-blue-800 hover:bg-blue-900 duration-200 md:py-2 md:text-lg md:px-14">
                         Create Job Field
