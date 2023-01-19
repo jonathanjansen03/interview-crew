6017774a3dd7ab9f9e56fa0aee685cca31a8132c
jonathanjansen03 <jonathan.jansen03@gmail.com>
2023-01-19 13:11:35 +0700

    feat: add interview list page

--- resources/views/includes/user-navbar.blade.php
+++ resources/views/includes/user-navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6">
         <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
             <div class="flex justify-start lg:w-0 lg:flex-1">
-                <a href="#">
+                <a href="/home">
                 <span class="sr-only">Logo</span>
                 <img class="h-8 w-auto sm:h-10" src="{{ asset('/images/logo.png') }}" alt="">
                 </a>
