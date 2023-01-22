3dc9dcfad5cdedf32676dd8491e18f5581cc56e4
jonathanjansen03 <jonathan.jansen03@gmail.com>
2023-01-22 20:04:09 +0700

    fix: interview history not wide enough bug

--- resources/views/user/interview-history.blade.php
+++ resources/views/user/interview-history.blade.php
@@ -9,55 +9,26 @@
     </div>
     
     <main class="relative flex justify-center">
-        <div class="flex flex-col justify-center items-center absolute bg-white drop-shadow-lg inset-y-24 w-3/6 h-fit py-10 px-6 rounded-xl">
+        <div class="flex flex-col justify-center items-center absolute bg-white drop-shadow-lg inset-y-12 w-4/6 h-fit py-10 px-6 rounded-xl">
             <h3 class="text-black font-bold text-2xl mb-3">Interview History</h3>
+            @foreach($interviews as $interview)
             <div class="grid grid-cols-2 gap-x-10 my-3">
-                <p class="font-medium align-middle">Username: Adipati_Dolken</p>
+                <p class="font-medium align-middle">Username: {{Auth::user()->full_name}}</p>
     
                 <p class="font-medium align-middle">
-                    Field: Data Scientist
+                    Field: {{$interview->field}}
                 </p>
                 
                 <p class="font-medium mt-3 align-middle">
-                    Time: 5th Jan 2023 11:00-12:00
+                    Time: {{$interview->date}}
                 </p>
 
                 <p class="font-medium mt-3 align-middle">
-                    Meeting Link: bit.ly/interview
+                    Meeting Link: {{$interview->link}}
                 </p>
             </div>
             <hr class="h-px my-5 bg-blue-300 border-0 w-11/12">
-            <div class="grid grid-cols-2 gap-x-10 mt-3">
-                <p class="font-medium align-middle">Username: Adipati_Dolken</p>
-    
-                <p class="font-medium align-middle">
-                    Field: Data Scientist
-                </p>
-                
-                <p class="font-medium mt-3 align-middle">
-                    Time: 5th Jan 2023 11:00-12:00
-                </p>
-
-                <p class="font-medium mt-3 align-middle">
-                    Meeting Link: bit.ly/interview
-                </p>
-            </div>
-            <hr class="h-px my-5 bg-blue-300 border-0 w-11/12">
-            <div class="grid grid-cols-2 gap-x-10 mt-3">
-                <p class="font-medium align-middle">Username: Adipati_Dolken</p>
-    
-                <p class="font-medium align-middle">
-                    Field: Data Scientist
-                </p>
-                
-                <p class="font-medium mt-3 align-middle">
-                    Time: 5th Jan 2023 11:00-12:00
-                </p>
-
-                <p class="font-medium mt-3 align-middle">
-                    Meeting Link: bit.ly/interview
-                </p>
-            </div>
+            @endforeach
         </div>
     </main>
 @endsection
